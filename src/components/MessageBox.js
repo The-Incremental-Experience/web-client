31ebb24103ec31fe7861a19f4dccb68acf3f47a4
UpstairsForest <nikita.batalov@uzh.ch>
2023-01-31 18:47:12 +0100

    Basic message-box styling to simplify development

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,5 +1,14 @@
+import { Box } from "@mui/material";
+import "../App.css";
+
 function MessageBox(props) {
-  return <div className={"MessageBox"}>{props.message.text}</div>;
+  return (
+    <Box className={"MessageBox"} sx={{ border: 1 }}>
+      <div className={"MessageText"} style={{ color: "grey" }}>
+        {props.message.text}
+      </div>
+    </Box>
+  );
 }
 
 export default MessageBox;
