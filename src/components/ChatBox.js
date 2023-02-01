5c2c2aed1e14c4c1137065df0e95fa54aa10cb86
UpstairsForest <nikita.batalov@uzh.ch>
2023-02-01 22:38:55 +0100

    Update chat-box to scroll to bottom automatically

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,8 +1,20 @@
-import "../Chat.css"
-import {Box} from "@mui/material";
+import "../Chat.css";
+import { Box } from "@mui/material";
+import { useEffect, useRef } from "react";
 
 function ChatBox(props) {
-  return <Box className={"ChatBox"}>{props.children}</Box>;
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [props.children]);
+
+  return (
+    <Box className={"ChatBox"}>
+      {props.children}
+      <div ref={messagesEndRef} />
+    </Box>
+  );
 }
 
 export default ChatBox;
